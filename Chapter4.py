61068c7e5c44bd0f30173c07f2d8b416962f0e52
Yusuke Shimizu <yusuk@cabim60031.lan>
2019-11-28 23:55:36 +0900

    Q37 solved

--- Chapter4.py
+++ Chapter4.py
@@ -3,6 +3,7 @@ def printQNum(num):
     print("# Q{}.".format(num))
     print("###########################\n")
 
+import re
 ###########################
 # 31.
 ###########################
@@ -114,9 +115,9 @@ printQNum(36)
 
 import json
 
+CiNii_BASE_URL = 'http://ci.nii.ac.jp/books/opensearch/search?'
 
 def getCiNiiSearchResult(q, f):
-    CiNii_BASE_URL = 'http://ci.nii.ac.jp/books/opensearch/search?'
     qparam = '&q=' + q
     fparam = '&format=' + f
     requestUrl = CiNii_BASE_URL + qparam + fparam
@@ -134,3 +135,42 @@ def getCiNiiSearchResult(q, f):
 
 for count, t in enumerate(getCiNiiSearchResult('お好み焼き', 'json')):
     print('{0}, {1}'.format(count+1, t))
+
+
+###########################
+# 37.
+###########################
+
+printQNum(37)
+
+def getTitleNumberperYear():
+    areaParam = '&area=22'
+    pageParam = '&p=1'
+    countParam = '&count=10000'
+    fParam = '&format=json'
+    requestUrl = CiNii_BASE_URL + areaParam + pageParam + countParam + fParam
+    con = requests.get(requestUrl)
+    queryJson = json.loads(con.text)
+
+    with open('./shizuokalib.json', 'w') as tempf:
+        tempf.write(json.dumps(queryJson, indent=2, ensure_ascii=False,))
+        print('done')
+
+    tempDict = {}
+    for n in range(1,10000):
+        try:
+            pub = queryJson['@graph'][0]['items'][n]['prism:publicationDate']
+        
+            if re.match(r'.*-.*', pub):
+                continue
+            elif pub in tempDict:
+                tempDict[pub] += 1
+            else:
+                tempDict.setdefault(pub, 1)
+        except KeyError as ke:
+            pass
+    
+    itemsPerYear = sorted(tempDict.items(), key = lambda x:x[0])
+    print(itemsPerYear)
+
+getTitleNumberperYear()
